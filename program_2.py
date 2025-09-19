ba312d59f5cdd1a2cc2ac76dcdf5c028e5115c3b
DevinGoshaw <DRGoshaw@students.unwsp.edu>
2025-09-19 10:57:32 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -10,8 +10,15 @@ def categorize_age(age):
     ######################
     # WRITE YOUR CODE HERE
     ######################
-
-
+    if age<=1:
+        print("he or she is an infant")
+    if age>1 and age<13:
+        print("he or she is a child")
+    if age>=13 and age<20:
+        print("he or she is a teenager")
+    if age>=20:
+        print ("he or she is an adult")
+    
     return ageCategory
 
 
